b6f961cbd73fc4519a8288310a82a48014bd1ba6
Luke A. Guest <laguest@archeia.com>
2019-08-29 21:12:55 +0100

    Refactor alien.py

--- basics/chapter_6/alien.py
+++ basics/chapter_6/alien.py
@@ -1,4 +1,5 @@
 alien_0 = { 'colour': 'green', 'points': 5 }
 
-print (alien_0['colour'])
-print (alien_0['points'])
+new_points = alien_0['points']
+
+print ("You just earned " + str (new_points) + " points!")
