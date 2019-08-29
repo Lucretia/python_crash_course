614a0a4bd06af12eb25014b16d6fb9e2045abaab
Luke A. Guest <laguest@archeia.com>
2019-08-29 15:02:48 +0100

    Add cars.py

---
+++ basics/chapter_3/cars.py
@@ -0,0 +1,23 @@
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+
+# cars.sort()
+
+# print (cars)
+
+# cars.sort(reverse=True)
+
+# print (cars)
+
+print ("Here's the original list:")
+print (cars)
+
+print ("Here's the sorted list:")
+print (sorted (cars))
+
+print ("Here's the original list again:")
+print (cars)
+
+cars.reverse()
+print (cars)
+
+print ("The length of cars is " + str (len (cars)))
