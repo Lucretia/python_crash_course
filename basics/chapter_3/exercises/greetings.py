6fb9dd1f44ffda31d731c99523f087d5a784c30e
Luke A. Guest <laguest@archeia.com>
2019-08-29 14:00:20 +0100

    Add exercise 3-2.

---
+++ basics/chapter_3/exercises/greetings.py
@@ -0,0 +1,7 @@
+names    = ['Darren', 'Rob', 'John', 'Paul']
+greeting = "Hello, "
+
+print (greeting + names[0])
+print (greeting + names[1])
+print (greeting + names[2])
+print (greeting + names[3])
