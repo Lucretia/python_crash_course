0f98efd04f84d4a133049a2d960372ab0169915d
Luke A. Guest <laguest@archeia.com>
2019-08-29 21:53:11 +0100

    Made a bit of a mess of the git history, this isn't a serous repo, so it doesn't really matter. Add another loop to output all the values with and without repeat entries.

--- basics/chapter_6/favourite_languages.py
+++ basics/chapter_6/favourite_languages.py
@@ -8,3 +8,21 @@ favourite_languages = {
 for name, language in favourite_languages.items():
     print (name.title() + "'s favourite language is " +
     language.title() + ".")
+
+print ("\nThe people who took the poll are:")
+
+for name in favourite_languages:#.keys():
+    print (name.title())
+
+for name in sorted (favourite_languages):
+    print (name.title() + ", thank you for taking hte poll.")
+
+print ("\nAll the languages (with repeats) in poll are:")
+
+for language in favourite_languages.values():
+    print (language.title())
+
+print ("\nAll the languages in poll are:")
+
+for language in set (favourite_languages.values()):
+    print (language.title())
