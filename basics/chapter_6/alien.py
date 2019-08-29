b6047a83d294101fcc916e2c030d44665b1db5f4
Luke A. Guest <laguest@archeia.com>
2019-08-29 21:19:31 +0100

    Refactor alien.py, change the alien's colour.

--- basics/chapter_6/alien.py
+++ basics/chapter_6/alien.py
@@ -6,3 +6,9 @@ alien_0['x_position'] = 0
 alien_0['y_position'] = 25
 
 print (alien_0)
+
+print ("The alien is " + alien_0['colour'] + ".")
+
+alien_0['colour'] = 'yellow'
+
+print ("The alien is now " + alien_0['colour'] + ".")
