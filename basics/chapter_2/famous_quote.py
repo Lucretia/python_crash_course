88fd975ab33503b9ba16f892c15e1c316370bded
Luke A. Guest <laguest@archeia.com>
2019-08-29 13:20:24 +0100

    Add 2-5. assign whole string to message and print that.

---
+++ basics/chapter_2/famous_quote.py
@@ -0,0 +1,5 @@
+quote          = "I thoroughly disapprove of duels. If a man should challenge me, I would take him kindly and forgivingly by the hand and lead him to a quiet place and kill him."
+famous_person  = "Mark Twain"
+message        = famous_person + ' once said, "' + quote + '"'
+
+print (message)
