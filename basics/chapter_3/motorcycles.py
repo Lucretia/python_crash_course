25cd6600c0fd5463868a9543c617c29d5d68a821
Luke A. Guest <laguest@archeia.com>
2019-08-29 14:39:25 +0100

    Add motorcycles.py

--- basics/chapter_3/motorcycles.py
+++ basics/chapter_3/motorcycles.py
@@ -17,3 +17,24 @@ print (motorcycles)
 del motorcycles[1]
 
 print (motorcycles)
+
+last_owned = motorcycles.pop()
+
+print ("The last motorcycle I owned was a " + last_owned.title() + ".")
+
+first_owned = motorcycles.pop(0)
+
+print ("The first motorcycle I owned was a " + first_owned.title() + ".")
+
+print (motorcycles)
+
+motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati']
+
+too_expensive = 'ducati'
+
+print (motorcycles)
+
+motorcycles.remove (too_expensive)
+
+print (motorcycles)
+print ("\nA " + too_expensive.title() + " is too expensive for me.")
