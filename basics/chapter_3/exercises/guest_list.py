97ebdb2370795ae64acbb7885343cf06753d9fd2
Luke A. Guest <laguest@archeia.com>
2019-08-29 14:48:05 +0100

    Add guest_list.py

---
+++ basics/chapter_3/exercises/guest_list.py
@@ -0,0 +1,8 @@
+guests  = ['Michelle Pfeiffer', 'Stephen Fry', 'Jessica Green', 'Alan Turing']
+message = "I'm formally inviting, "
+suffix  = ", to dinner"
+
+print (message + guests[0] + suffix)
+print (message + guests[1] + suffix)
+print (message + guests[2] + suffix)
+print (message + guests[3] + suffix)
