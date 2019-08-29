e4ea9c24e69b056d3b5ad7a78d666bfa347899b3
Luke A. Guest <laguest@archeia.com>
2019-08-29 14:03:03 +0100

    Add exercise 3-3.

---
+++ basics/chapter_3/exercises/own_list.py
@@ -0,0 +1,5 @@
+vehicles = ['bus', 'Aston Martin DB9', 'Ford Fiesta']
+
+print ("I don't like travelling on the " + vehicles[0])
+print ("I would like to drive an " + vehicles[1])
+print ("My first car was a " + vehicles[2])
