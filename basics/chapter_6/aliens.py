194235fbb182e93ba948fb7267d093ff43514af0
Luke A. Guest <laguest@archeia.com>
2019-08-29 22:05:48 +0100

    Refactor aliens.py to create aliens in a loop.

--- basics/chapter_6/aliens.py
+++ basics/chapter_6/aliens.py
@@ -2,7 +2,24 @@ alien_0 = { 'colour': 'green',  'points':  5 }
 alien_1 = { 'colour': 'yellow', 'points': 10 }
 alien_2 = { 'colour': 'red',    'points': 15 }
 
-aliens = [alien_0, alien_1, alien_2]
+aliens = []
 
-for alien in aliens:
+for alien in range(30):
+    new_alien = { 'colour': 'green', 'points': 5, 'speed': 'slow' }
+    aliens.append (new_alien)
+
+for alien in aliens[0:3]:
+    if alien['colour'] == 'green':
+        alien['colour'] = 'yellow'
+        alien['speed']  = 'medium'
+        alien['points'] = 10
+    elif alien['colour'] == 'yellow':
+        alien['colour'] = 'red'
+        alien['speed']  = 'fast'
+        alien['points'] = 15
+
+for alien in aliens[:5]:
     print (alien)
+print ("...")
+
+print ("Total number of aliens: " + str (len (aliens)))
