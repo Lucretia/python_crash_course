1243c066071cf705e7f0a3d2f0d77796d03f77e5
Luke A. Guest <laguest@archeia.com>
2019-08-29 13:56:01 +0100

    Change bicycles.py to print a specific bike out.

--- basics/chapter_3/bicycles.py
+++ basics/chapter_3/bicycles.py
@@ -1,8 +1,5 @@
 bicycles = ['trek', 'cannondale', 'redline', 'specialized']
 
-# print (bicycles)
-print (bicycles[0].title())
-print (bicycles[1].title())
-print (bicycles[-1].title())
-print (bicycles[-2].title())
-print (bicycles[-3].title())
+message = "My first bicycle was a " + bicycles[0].title() + "."
+
+print (message)
