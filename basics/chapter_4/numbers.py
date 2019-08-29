38eb685f5ee70b8efa9bd4f7a13ff447e0a786ac
Luke A. Guest <laguest@archeia.com>
2019-08-29 16:06:55 +0100

    Add even_numbers and squares.

--- basics/chapter_4/numbers.py
+++ basics/chapter_4/numbers.py
@@ -3,3 +3,10 @@ for value in range(1, 6):
 
 numbers = list (range (1, 6))
 print (numbers)
+
+even_numbers = list (range (2, 11, 2))
+
+print (even_numbers)
+
+squares = [value ** 2 for value in range (1, 11)]
+print (squares)
