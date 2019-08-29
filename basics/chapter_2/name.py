150acf8f8c5a62abccb044486dc52432b0b3abd7
Luke A. Guest <laguest@archeia.com>
2019-08-29 13:03:34 +0100

    Add name.py

---
+++ basics/chapter_2/name.py
@@ -0,0 +1,19 @@
+name = "ada lovelace"
+name = name.title()
+print (name)
+
+name = name.upper()
+print (name)
+
+name = name.lower()
+print (name)
+
+first_name = "ada"
+last_name  = "lovelace"
+full_name  = first_name + " " + last_name
+
+print (full_name)
+
+message = "Hello, " + full_name.title() + "!"
+
+print (message)
